cab93567d0f581f2dbc568496f160743a327822f
omar97hassan <37211092+omar97hassan@users.noreply.github.com>
2019-04-08 17:10:48 +0200

    esp4motori

--- Motorcont/MotorTester/MotorTester.ino
+++ Motorcont/MotorTester/MotorTester.ino
@@ -1,18 +1,29 @@
-#include<Servo.h>
-Servo motor[4];
-int i;
-int potPin=A2;
-int cntlPin=7;
+#include<ESP32Servo.h>
+
+const int potPin=34;
+const int cntlPin=11;
+const int motorPin[]={33,32,23,19};
+
 const int noMotors=4;
+const int adcMax=4095;
+
 const int lbnd[]={1735,1605,1605,1615};
 const int ubnd[]={1850,2150,1710,2150};
+
+Servo motor[noMotors];
+
+int i;
+
+void resetMotors();
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(cntlPin,INPUT_PULLUP);
-  motor[0].attach(3);
-  motor[1].attach(5);
-  motor[2].attach(6);
-  motor[3].attach(9);
+  pinMode(potPin,INPUT);
+  for(i=0;i<noMotors;i++){
+    motor[i].attach(motorPin[i]);
+  }
+  resetMotors();
   for(i=0;i<noMotors;i++){
     motor[i].writeMicroseconds(lbnd[i]);
   }
@@ -23,18 +34,25 @@ long throtle,tempMap,potVal;
 void loop() {
   // put your main code here, to run repeatedly:
   potVal=analogRead(potPin);
-  throtle=map(throtle,0,1023,0,100);
+  throtle=map(throtle,0,adcMax,0,100);
   if(digitalRead(cntlPin)){
-    
+    resetMotors();
   }
   else{
     for(i=0;i<noMotors;i++){
-      tempMap=map(potVal,0,1023,lbnd[i],ubnd[i]);
+      tempMap=map(potVal,0,adcMax,lbnd[i],ubnd[i]);
       motor[i].writeMicroseconds(tempMap);
     }
   }
 //  Serial.println();
 //  Serial.println(bl);
-//  Serial.println(bll);
+//  Serial.println(bll)..;
   delay(200);
+}
+
+void resetMotors(){
+  for(i=0;i<noMotors;i++){
+    motor[i].writeMicroseconds(1000);
+  }
+  delay(2000);
 }
