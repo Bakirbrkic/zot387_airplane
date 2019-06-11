de148fa9901ed5c40733962f40299919949ed62a
omar97hassan <37211092+omar97hassan@users.noreply.github.com>
2019-06-11 14:53:31 +0200

    .

--- Megaplane/src/main.cpp
+++ Megaplane/src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
+#include <BME280I2C.h>
 #include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BMP280.h>
 
 int getDist(const int, const int);
 void namaSerial();
@@ -10,22 +9,37 @@ void espuSerial();
 const int trigPin = 35;
 const int echoPin = 34;
 
+BME280I2C tandp;
+BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
+BME280::PresUnit presUnit(BME280::PresUnit_Pa);
+
 void setup() {
 	pinMode(trigPin, OUTPUT);
 	pinMode(echoPin, INPUT);
 	Serial1.begin(115200);
 	Serial.begin(9600);
+	if(!tandp.begin()) {
+		Serial.println("Could not find a valid BME280 sensor, check wiring!");
+		while (1);
+	}
+	switch(tandp.chipModel()){
+		case BME280::ChipModel_BME280:
+		Serial.println("Found BME280 sensor! Success.");
+		break;
+		case BME280::ChipModel_BMP280:
+		Serial.println("Found BMP280 sensor! No Humidity available.");
+		break;
+		default:
+		Serial.println("Found UNKNOWN sensor! Error!");
+	}
 }
 
 int dist;
-double temp,pres,alt;
-Adafruit_BMP280 tandp;
+float temp,pres,alt,hum;
 
 void loop() {
 	dist=getDist(trigPin,echoPin);
-	temp=tandp.readTemperature();
-	pres=tandp.readPressure();
-	alt=tandp.readAltitude(1013.25);
+	tandp.read(pres, temp, hum, tempUnit, presUnit);
 	namaSerial();
 	espuSerial();
 	delay(200);
@@ -54,9 +68,9 @@ void namaSerial(){
     Serial.print(pres);
     Serial.println(" Pa");
 
-    Serial.print("Approx altitude = ");
-    Serial.print(alt); // this should be adjusted to your local forcase
-    Serial.println(" m");
+    Serial.print("Humidity = ");
+    Serial.print(hum); // this should be adjusted to your local forcase
+    Serial.println(" %");
 }
 
 void espuSerial(){
